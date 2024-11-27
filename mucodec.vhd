a456452564e83b30ab69f4f7ec03ba559b474cb0
Jasminder-Singh <94368641+GodOfjiz@users.noreply.github.com>
2024-11-27 14:27:05 +0800

    Update mucodec.vhd

--- mucodec.vhd
+++ mucodec.vhd
@@ -1,32 +1,38 @@
-LIBRARY ieee;
+----------------------------------------------------------------------------------
+-- Company: Computer Architecture and System Research (CASR), HKU, Hong Kong
+-- Engineer: Mo Song
+-- 
+-- Create Date: 09/09/2022 06:20:56 PM
+-- Design Name: system top
+-- Module Name: top - Behavioral
+-- Project Name: Music Decoder
+-- Target Devices: Xilinx Basys3
+-- Tool Versions: Vivado 2022.1
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
+LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-
-
-entity mucodec is
-
+entity mcdecoder is
     port (
-
         din     : IN std_logic_vector(2 downto 0);
-
         valid   : IN std_logic;
-
         clr     : IN std_logic;
-
         clk     : IN std_logic;
-
         dout    : OUT std_logic_vector(7 downto 0);
-
         dvalid  : OUT std_logic;
-
         error   : OUT std_logic);
+end mcdecoder;
 
-end mucodec;
-
-
-
-architecture Behavioral of mucodec is
+architecture Behavioral of mcdecoder is
 
     type state_type is (St_RESET, St_ERROR,
 
@@ -44,7 +50,7 @@ architecture Behavioral of mucodec is
 
     signal state, next_state : state_type := St_RESET;
 
-    signal tmp_error, tmp_dvalid : std_logic := '0';
+    signal tmp_error: std_logic := '0';
 
     
 
@@ -57,10 +63,7 @@ BEGIN
     BEGIN
 
         IF clr = '1' THEN
-
             state <= St_RESET;
-
-
 
         ELSIF rising_edge(clk) THEN
 
@@ -180,49 +183,25 @@ BEGIN
 
                         next_state <= St_A;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="011") then
 
                         next_state <= St_B;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="100") then
 
                         next_state <= St_C;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="101") then
 
                         next_state <= St_D;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="110") then
 
                         next_state <= St_E;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if;           
 
@@ -234,49 +213,25 @@ BEGIN
 
                         next_state <= St_F;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="011") then
 
                         next_state <= St_G;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="100") then
 
                         next_state <= St_H;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="101") then
 
                         next_state <= St_I;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="110") then
 
                         next_state <= St_J;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if; 
 
@@ -288,49 +243,25 @@ BEGIN
 
                         next_state <= St_K;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="010") then
 
                         next_state <= St_L;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="100") then
 
                         next_state <= St_M;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="101") then
 
                         next_state <= St_N;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="110") then
 
                         next_state <= St_O;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if; 
 
@@ -342,49 +273,25 @@ BEGIN
 
                         next_state <= St_P;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="010") then
 
                         next_state <= St_Q;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="011") then
 
                         next_state <= St_R;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="101") then
 
                         next_state <= St_S;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="110") then
 
                         next_state <= St_T;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if; 
 
@@ -396,49 +303,25 @@ BEGIN
 
                         next_state <= St_U;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="010") then
 
                         next_state <= St_V;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="011") then
 
                         next_state <= St_W;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="100") then
 
                         next_state <= St_X;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="110") then
 
                         next_state <= St_Y;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if; 
 
@@ -450,49 +333,25 @@ BEGIN
 
                         next_state <= St_Z;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="010") then
 
                         next_state <= St_EXCLAMATION;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="011") then
 
                         next_state <= St_PERIOD;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     elsif (din="100") then
 
                         next_state <= St_QUESTION;
-
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
 
                     elsif (din="101") then
 
                         next_state <= St_SPACE;
 
-                        tmp_dvalid <= '1';
-
-                        tmp_error <= '0';
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if; 
 
@@ -504,17 +363,9 @@ BEGIN
 
                         next_state <= St_70;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0'; 
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if;
 
@@ -526,17 +377,9 @@ BEGIN
 
                         next_state <= St_707;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0'; 
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if;
 
@@ -548,17 +391,9 @@ BEGIN
 
                         next_state <= St_7070;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0'; 
-
                     else
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if;
 
@@ -570,17 +405,9 @@ BEGIN
 
                         next_state <= St_0;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0'; 
-
                     else
 
                         next_state <= St_RESET;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
 
                     end if;
 
@@ -594,77 +421,39 @@ BEGIN
 
                         next_state <= St_1;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0'; 
-
                     elsif (din="010") then
 
                         next_state <= St_2;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
 
                     elsif (din="011") then
 
                         next_state <= St_3;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
-
                     elsif (din="100") then
 
                         next_state <= St_4;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
 
                     elsif (din="101") then
 
                         next_state <= St_5;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
-
                     elsif (din="110") then
 
                         next_state <= St_6;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
 
                     elsif (din="111") then
 
                         next_state <= St_7;
 
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '0';
-
                     else --does this else statement need to be included? All cases have been considered (0 through 7), so it seems redundant
 
                         next_state <= St_ERROR;
-
-                        tmp_dvalid <= '0';
-
-                        tmp_error <= '1';
 
                     end if;
 
                 end case;
 
         end if;
-
-            dvalid <= tmp_dvalid;
-
-    error <= tmp_error;
-
-
 
     end process;
 
@@ -676,79 +465,73 @@ BEGIN
 
         CASE state IS
 
-            WHEN St_A   => dout <= "01000001"; -- ASCII for 'A'
+            WHEN St_A   => dout <= "01000001"; dvalid<='1'; error <= '0'; -- ASCII for 'A'
 
-            WHEN St_B   => dout <= "01000010"; -- ASCII for 'B'
+            WHEN St_B   => dout <= "01000010"; dvalid<='1'; error <= '0';  -- ASCII for 'B'
 
-            WHEN St_C   => dout <= "01000011"; -- ASCII for 'C'
+            WHEN St_C   => dout <= "01000011"; dvalid<='1'; error <= '0';   -- ASCII for 'C'
 
-            WHEN St_D   => dout <= "01000100"; -- ASCII for 'D'
+            WHEN St_D   => dout <= "01000100"; dvalid<='1'; error <= '0';   -- ASCII for 'D'
 
-            WHEN St_E   => dout <= "01000101"; -- ASCII for 'E'
+            WHEN St_E   => dout <= "01000101"; dvalid<='1'; error <= '0';  -- ASCII for 'E'
 
-            WHEN St_F   => dout <= "01000110"; -- ASCII for 'F'
+            WHEN St_F   => dout <= "01000110"; dvalid<='1'; error <= '0';   -- ASCII for 'F'
 
-            WHEN St_G   => dout <= "01000111"; -- ASCII for 'G'
+            WHEN St_G   => dout <= "01000111"; dvalid<='1'; error <= '0';   -- ASCII for 'G'
 
-            WHEN St_H   => dout <= "01001000"; -- ASCII for 'H'
+            WHEN St_H   => dout <= "01001000"; dvalid<='1'; error <= '0';   -- ASCII for 'H'
 
-            WHEN St_I   => dout <= "01001001"; -- ASCII for 'I'
+            WHEN St_I   => dout <= "01001001"; dvalid<='1'; error <= '0';   -- ASCII for 'I'
 
-            WHEN St_J   => dout <= "01001010"; -- ASCII for 'J'
+            WHEN St_J   => dout <= "01001010"; dvalid<='1'; error <= '0';   -- ASCII for 'J'
 
-            WHEN St_K   => dout <= "01001011"; -- ASCII for 'K'
+            WHEN St_K   => dout <= "01001011"; dvalid<='1'; error <= '0';   -- ASCII for 'K'
 
-            WHEN St_L   => dout <= "01001100"; -- ASCII for 'L'
+            WHEN St_L   => dout <= "01001100"; dvalid<='1'; error <= '0';   -- ASCII for 'L'
 
-            WHEN St_M   => dout <= "01001101"; -- ASCII for 'M'
+            WHEN St_M   => dout <= "01001101"; dvalid<='1'; error <= '0';   -- ASCII for 'M'
 
-            WHEN St_N   => dout <= "01001110"; -- ASCII for 'N'
+            WHEN St_N   => dout <= "01001110"; dvalid<='1'; error <= '0';   -- ASCII for 'N'
 
-            WHEN St_O   => dout <= "01001111"; -- ASCII for 'O'
+            WHEN St_O   => dout <= "01001111"; dvalid<='1'; error <= '0';   -- ASCII for 'O'
 
-            WHEN St_P   => dout <= "01010000"; -- ASCII for 'P'
+            WHEN St_P   => dout <= "01010000"; dvalid<='1'; error <= '0';   -- ASCII for 'P'
 
-            WHEN St_Q   => dout <= "01010001"; -- ASCII for 'Q'
+            WHEN St_Q   => dout <= "01010001"; dvalid<='1'; error <= '0';   -- ASCII for 'Q'
 
-            WHEN St_R   => dout <= "01010010"; -- ASCII for 'R'
+            WHEN St_R   => dout <= "01010010"; dvalid<='1'; error <= '0';   -- ASCII for 'R'
 
-            WHEN St_S   => dout <= "01010011"; -- ASCII for 'S'
+            WHEN St_S   => dout <= "01010011"; dvalid<='1'; error <= '0';   -- ASCII for 'S'
 
-            WHEN St_T   => dout <= "01010100"; -- ASCII for 'T'
+            WHEN St_T   => dout <= "01010100"; dvalid<='1'; error <= '0';   -- ASCII for 'T'
 
-            WHEN St_U   => dout <= "01010101"; -- ASCII for 'U'
+            WHEN St_U   => dout <= "01010101"; dvalid<='1'; error <= '0';   -- ASCII for 'U'
 
-            WHEN St_V   => dout <= "01010110"; -- ASCII for 'V'
+            WHEN St_V   => dout <= "01010110"; dvalid<='1'; error <= '0';   -- ASCII for 'V'
 
-            WHEN St_W   => dout <= "01010111"; -- ASCII for 'W'
+            WHEN St_W   => dout <= "01010111"; dvalid<='1'; error <= '0';   -- ASCII for 'W'
 
-            WHEN St_X   => dout <= "01011000"; -- ASCII for 'X'
+            WHEN St_X   => dout <= "01011000"; dvalid<='1'; error <= '0';   -- ASCII for 'X'
 
-            WHEN St_Y   => dout <= "01011001"; -- ASCII for 'Y'
+            WHEN St_Y   => dout <= "01011001"; dvalid<='1'; error <= '0';   -- ASCII for 'Y'
 
-            WHEN St_Z   => dout <= "01011010"; -- ASCII for 'Z'
+            WHEN St_Z   => dout <= "01011010"; dvalid<='1'; error <= '0';   -- ASCII for 'Z'
 
-            WHEN St_SPACE => dout <= "00100000"; -- ASCII for 'Space'
+            WHEN St_SPACE => dout <= "00100000"; dvalid<='1'; error <= '0';   -- ASCII for 'Space'
 
-            WHEN St_EXCLAMATION => dout <= "00100001"; -- ASCII for '!'
+            WHEN St_EXCLAMATION => dout <= "00100001"; dvalid<='1'; error <= '0';   -- ASCII for '!'
 
-            WHEN St_PERIOD => dout <= "00101110"; -- ASCII for '.'
+            WHEN St_PERIOD => dout <= "00101110"; dvalid<='1'; error <= '0';   -- ASCII for '.'
 
-            WHEN St_QUESTION => dout <= "00111111"; -- ASCII for '?'
-
+            WHEN St_QUESTION => dout <= "00111111"; dvalid<='1'; error <= '0';   -- ASCII for '?'
             
+            WHEN St_ERROR => dout <= "00000000"; dvalid<='0'; error <= '1'; -- Error state
 
-            WHEN OTHERS => dout <= "00000000"; -- Default to zero
-
-
+            WHEN OTHERS => dout <= "00000000"; dvalid<='0'; error <= '0';   -- Default to zero
 
         END CASE;
 
     END PROCESS;
-
-    
-
-    
 
 end Behavioral;
 
